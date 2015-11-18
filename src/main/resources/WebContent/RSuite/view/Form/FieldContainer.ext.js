e8e65a1c6a7451087c45ced6ac0de51dd32a4e26
Bryan Elliott <belliott@rsicms.com>
2015-11-18 15:08:32 -0500

    Prevented overload of built-in implementation of multiselect[size=1] -> select2

--- src/main/resources/WebContent/RSuite/view/Form/FieldContainer.ext.js
+++ src/main/resources/WebContent/RSuite/view/Form/FieldContainer.ext.js
@@ -1,9 +1,19 @@
-RSuite.view.Form.FieldContainer.reopen({
-	formControlType: function () {
-		var type = (this.get("model.formControlType") || this.get("model.dataType.formControlType") || "text");
-		if (type === 'multiselect' && parseInt(this.get('model.size')) === 1) {
-			return 'select2';
+(function () {
+	var tmp = RSuite.view.Form.FieldContainer.create({
+		model: {
+			formControlType: 'multiselect',
+			size: 1
 		}
-		return type;
-	}.property('model.formControlType', 'model.dataType.formControlType', 'model.size')
-});
+	});
+	if (tmp.get('formControlType') !== 'select2') {
+		RSuite.view.Form.FieldContainer.reopen({
+			formControlType: function () {
+				var type = (this.get("model.formControlType") || this.get("model.dataType.formControlType") || "text");
+				if (type === 'multiselect' && parseInt(this.get('model.size')) === 1) {
+					return 'select2';
+				}
+				return type;
+			}.property('model.formControlType', 'model.dataType.formControlType', 'model.size')
+		});
+	}
+}());
