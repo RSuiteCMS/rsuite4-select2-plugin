533002bfd76f2fc15233a3c5db67628cdf5d9efe
Bryan Elliott <belliott@rsicms.com>
2015-11-18 14:48:46 -0500

    Removed build artifacts

--- build/resources/main/WebContent/RSuite/view/Form/FieldContainer.ext.js
+++
@@ -1,9 +0,0 @@
-RSuite.view.Form.FieldContainer.reopen({
-	formControlType: function () {
-		var type = (this.get("model.formControlType") || this.get("model.dataType.formControlType") || "text");
-		if (type === 'multiselect' && parseInt(this.get('model.size')) === 1) {
-			return 'select2';
-		}
-		return type;
-	}.property('model.formControlType', 'model.dataType.formControlType', 'model.size')
-});
